d361483e8538fdcd6a98f9d062da3670fb66d752
S1rirat <sirirat.soukpi@noseryoung.com>
2023-01-11 16:24:30 +0100

    5

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ closeCart.onclick = () => {
     cart.classList.remove("active");
 }
 
+
 // Cart Working JS
 if (document.readyState == "loading"){
     document.addEventListener("DOMContentLoaded", ready);
@@ -31,9 +32,15 @@ function ready(){
     }
     // Quantity Changes
     var quantityInputs = document.getElementsByClassName("cart-quantity");
-    For (var i = 0; i < quantityInputs.length; i++){
+    for (var i = 0; i < quantityInputs.length; i++){
         var input = quantityInputs[i];
         input.addEventListener("change", quantityChanged);
+    }
+    // Add To Cart
+    var addCart = document.getElementsByClassName("add-cart");
+    for (var i = 0; i < addCart.length; i++) {
+        var button = addCart[i];
+        button.addEventListener("click", addcartClicked);
     }
 }
 
@@ -45,26 +52,50 @@ function removeCartItem(event){
 }
 
 // Quantity Changes
-functions quantityChanged(event){
+function quantityChanged(event){
     var input = event.target;
+    // Total Preis
     if (isNaN(input.value) || input.value <= 0){
         input.value = 1;
     }
+    updatetotal();
 }
+
+// Add To Cart
+function addcartClicked(event){
+    var button= event.target;
+    var shopProducts = button.parentElement;
+    var title = shopProducts.getElementsByClassName("product-title")[0].innerText;
+    var price = shopProducts.getElementsByClassName("price")[0].innerText;
+    var productImg = shopProducts.getElementsByClassName("product-img")[0].src;
+    addProductToCart(title, price, productImg);
+    updatetotal();
+}
+function addProductToCart(title, price, productImg){
+    var cartShopBox = document.createElement("div");
+    cartShopBox.classList.add("cart-box");
+    var cartItems = document.getElementsByClassName("cart-content")[0];
+    var cartItemsNames = cartItems.getElementsByClassName("cart-product-title");
+    for (var i = 0; i < cartItemsNames.length; i++) {
+        // Already added
+        alert("You have already add this item to cart");
+    }
+}
+
 
 // Update Total
 function updatetotal(){
     var cartContent = document.getElementsByClassName("cart-content")[0];
     var cartBoxes = cartContent.getElementsByClassName("cart-box");
     var total = 0;
-    for (var i = 0; i < removeCartButtons.length; i++){
+    for (var i = 0; i < cartBoxes.length; i++){
         var cartBox = cartBoxes[i];
         var priceElement = cartBox.getElementsByClassName("cart-price")[0];
         var quantityElement = cartBox.getElementsByClassName("cart-quantity")[0];
-        var price = parseFloat(priceElement.innerText.replace("CHF", ""));
-        var quantity = quantityElement.value;
+        var price = parseFloat(priceElement.innerText.replace("CHF"," "));
+        var quantity = quantityElement.ariValueMay;
         total = total + (price * quantity);
-        // Update Total
+
 
 
         document.getElementsByClassName("total-price")[0].innerText = "CHF" + total;
